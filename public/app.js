460e4f53eb585d632a4a0ceb3563e79b8876b6d4
ChrisZeag1 <chris.zeag@gmail.com>
2016-12-12 13:17:53 -0600

    Delete app.js

--- public/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var Helloworld = (function () {
-    function Helloworld() {
-        console.log('asdadds');
-    }
-    return Helloworld;
-}());
-exports.Helloworld = Helloworld;
-;
