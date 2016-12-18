fe33d39ca2a44cf428e74603e776828ada7ebe28
Christian Zepeda <c.zepeda.aguirre@accenture.com>
2016-12-18 00:26:13 -0600

    set heroku local with procfile

--- server.js
+++ server.js
@@ -7,9 +7,6 @@ app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
 
-app.get('/', function(request, response) {
-  response.render('public/index.html');
-});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
