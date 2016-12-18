255d9736d3b3e354aa32e3eefa34debdbcd3ab2e
Christian Zepeda <c.zepeda.aguirre@accenture.com>
2016-12-18 00:19:02 -0600

    change server

--- server.js
+++ server.js
@@ -6,11 +6,9 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.render('public/index.html');
 });
 
 app.listen(app.get('port'), function() {
