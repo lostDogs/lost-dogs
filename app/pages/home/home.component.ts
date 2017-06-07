2c1453959be6bc51d98ab9b677e85899737b63e5
Christian Zepeda <c.zepeda.aguirre@accenture.com>
2017-06-06 19:41:49 -0500

    added scroll listener on homepage

--- app/pages/home/home.component.ts
+++ app/pages/home/home.component.ts
@@ -1,11 +1,18 @@
-import { Component} from '@angular/core';
+import { Component, HostListener, Inject} from '@angular/core';
+import { DOCUMENT } from '@angular/platform-browser';
 
 @Component({
   selector: 'home',
   template: require('./home.template.html'),
   styles: [ require('./_home-style.scss')]
 })
+
 export class homeComponent {
   public dir: string;
-  constructor () { }
+  constructor (@Inject(DOCUMENT) private document:  Document) { }
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    let number = this.document.body.scrollTop;
+    console.log('Scrolling', number);
+  }  
 };
