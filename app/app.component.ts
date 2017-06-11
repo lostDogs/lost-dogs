2e2ebe5beaefe93e269bcf3feaeb9bf59b1987f7
Christian Zepeda <c.zepeda.aguirre@accenture.com>
2017-06-10 21:44:28 -0500

    footer and content added

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,7 +7,7 @@ import 'materialize-css/bin/materialize.css';
 @Component({
   selector: 'my-app',
   encapsulation: ViewEncapsulation.None,
-  template: '<general-header class="row"></general-header><router-outlet></router-outlet><div>{{footer}}</div>',
+  template: '<general-header class="row"></general-header><router-outlet></router-outlet><general-footer></general-footer>',
   styles: [require('./main.scss')]
 })
 export class appComponent {
--- app/app.module.ts
+++ app/app.module.ts
@@ -3,10 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import {AppRouterModule, routerComponents} from './app.router';
 import {appComponent} from './app.component';
 import  {generalHeaderComponent} from './common/components/header/general-header.component';
-
+import {generalFooterComponent} from './common/components/footer/general-footer.component';
 @NgModule({
   imports: [BrowserModule, AppRouterModule],
-  declarations: [appComponent, routerComponents, generalHeaderComponent ],
+  declarations: [appComponent, routerComponents, generalHeaderComponent, generalFooterComponent],
   bootstrap:    [appComponent]
 })
 export class AppModule {}
