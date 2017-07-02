ec17c8ac0297532d6b91223c908215f8724edc9c
Christian Zepeda <c.zepeda.aguirre@accenture.com>
2017-07-02 14:53:37 -0500

    login popover style finish

--- app/common/components/header/general-header.component.ts
+++ app/common/components/header/general-header.component.ts
@@ -1,13 +1,19 @@
-import { Component,Directive, OnInit} from '@angular/core';
+import {Component,Directive, OnInit} from '@angular/core';
 
 @Component({
   selector: 'general-header',
   template: require('./general-header.template.html'),
   styles: [ require('./_general-header.scss')],
-
 })
+
 export class generalHeaderComponent implements OnInit  {
+  public showLoginFrom : boolean;
+  public offsetY: string;
   constructor () {
+  }
+  public toggleLoginFrom(event: any) {
+    this.offsetY = 50 + event.pageY- event.offsetY + 'px';
+    this.showLoginFrom = !this.showLoginFrom;    
   }
 
   ngOnInit() {
