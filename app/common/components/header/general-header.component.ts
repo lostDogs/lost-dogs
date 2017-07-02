a8b93e2a4432a2a9562e02d61fad78348c9a62ac
Christian Zepeda <c.zepeda.aguirre@accenture.com>
2017-07-02 15:41:57 -0500

    login popover hiding when clicking outside

--- app/common/components/header/general-header.component.ts
+++ app/common/components/header/general-header.component.ts
@@ -1,4 +1,4 @@
-import {Component,Directive, OnInit} from '@angular/core';
+import {ElementRef, Renderer, Component,Directive, OnInit} from '@angular/core';
 
 @Component({
   selector: 'general-header',
@@ -9,7 +9,14 @@ import {Component,Directive, OnInit} from '@angular/core';
 export class generalHeaderComponent implements OnInit  {
   public showLoginFrom : boolean;
   public offsetY: string;
-  constructor () {
+  constructor (public renderer: Renderer, public elRef: ElementRef) {
+    this.renderer.listenGlobal('document', 'click', (event: any) => {
+      const loginDom: any = this.elRef.nativeElement.childNodes[0].childNodes[5].childNodes[1].childNodes[1];
+      if(this.showLoginFrom && !(this.elRef.nativeElement.lastChild.contains(event.target) || loginDom.contains(event.target))) {
+        this.showLoginFrom = false;
+      }
+    });
+
   }
   public toggleLoginFrom(event: any) {
     this.offsetY = 50 + event.pageY- event.offsetY + 'px';
