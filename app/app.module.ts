cb53ccb2245e2e1e604618c7fab4db539720758c
Christian Zepeda <c.zepeda.aguirre@accenture.com>
2017-07-06 22:01:44 -0500

    added scroll service

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,9 +4,11 @@ import {AppRouterModule, routerComponents} from './app.router';
 import {appComponent} from './app.component';
 import  {generalHeaderComponent} from './common/components/header/general-header.component';
 import {generalFooterComponent} from './common/components/footer/general-footer.component';
+import {ScrollService} from './common/components/services/scroll.service';
 @NgModule({
   imports: [BrowserModule, AppRouterModule],
   declarations: [appComponent, routerComponents, generalHeaderComponent, generalFooterComponent],
+  providers:  [{ provide: ScrollService, useClass: ScrollService }],
   bootstrap:    [appComponent]
 })
 export class AppModule {}
--- app/common/components/header/general-header.component.ts
+++ app/common/components/header/general-header.component.ts
@@ -1,4 +1,5 @@
 import {ElementRef, Renderer, Component,Directive, OnInit} from '@angular/core';
+import {ScrollService} from '../services/scroll.service';
 
 @Component({
   selector: 'general-header',
@@ -9,17 +10,16 @@ import {ElementRef, Renderer, Component,Directive, OnInit} from '@angular/core';
 export class generalHeaderComponent implements OnInit  {
   public showLoginFrom : boolean;
   public offsetY: string;
-  constructor (public renderer: Renderer, public elRef: ElementRef) {
+  constructor (public renderer: Renderer, public elRef: ElementRef, public ScrollService: ScrollService) {
     this.renderer.listenGlobal('document', 'click', (event: any) => {
       const loginDom: any = this.elRef.nativeElement.childNodes[0].childNodes[5].childNodes[1].childNodes[1];
-      if(this.showLoginFrom && !(this.elRef.nativeElement.lastChild.contains(event.target) || loginDom.contains(event.target))) {
+      if (this.showLoginFrom && !(this.elRef.nativeElement.lastChild.contains(event.target) || loginDom.contains(event.target))) {
         this.showLoginFrom = false;
       }
     });
 
   }
   public toggleLoginFrom(event: any) {
-    this.offsetY = 50 + event.pageY- event.offsetY + 'px';
     this.showLoginFrom = !this.showLoginFrom;    
   }
 
