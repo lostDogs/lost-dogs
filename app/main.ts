a36a1724b98bdf72278ceb9a491a8e73734cc676
Chris Zepeda <chris.zeag@gmail.com>
2017-08-21 21:17:03 -0500

    enableprodMod angular in main

--- app/main.ts
+++ app/main.ts
@@ -3,5 +3,6 @@ import 'reflect-metadata';
 import 'zone.js/dist/zone';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {AppModule} from './app.module';
-
+import {enableProdMode} from '@angular/core';
+enableProdMode();
 platformBrowserDynamic().bootstrapModule(AppModule);
