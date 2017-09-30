5168e7b85fe3031820e692736fa7df93c597726c
Chris Zepeda <chris.zeag@gmail.com>
2017-09-29 19:06:39 -0500

    added scroll button home page and links to lost found

--- app/pages/home/home.component.ts
+++ app/pages/home/home.component.ts
@@ -1,7 +1,7 @@
-import { Component, HostListener, Inject} from '@angular/core';
+import { Component, HostListener, Inject, ElementRef, ViewChild} from '@angular/core';
 import { DOCUMENT } from '@angular/platform-browser';
 import {GlobalFunctionService} from '../../common/services/global-function.service';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import { Location} from '@angular/common';
 import {UserService} from '../../common/services/user.service';
 
@@ -14,17 +14,28 @@ import {UserService} from '../../common/services/user.service';
 export class homeComponent {
   public scrollnormalize: number;
   public newUser: boolean;
+  public scrolldownDisplay: boolean = true;
+  public scrollMax: number;
+  @ViewChild('Description')
+  public descriptionDom: ElementRef;
 
-  constructor (@Inject(DOCUMENT) private document:  Document, public globalService : GlobalFunctionService, public activatedRoute: ActivatedRoute, public location: Location, public userService: UserService) {}
+  constructor (@Inject(DOCUMENT) private document:  Document, public globalService : GlobalFunctionService, public activatedRoute: ActivatedRoute, public location: Location, public userService: UserService, public router: Router) {
+    this.scrollMax = (window.innerHeight - (window.innerHeight / 8)) / 2;
+  }
   @HostListener('window:scroll', ['$event'])
   onWindowScroll() {
       const scrollMax: number = (window.innerHeight - (window.innerHeight / 8)) / 2;
+      this.scrollMax = scrollMax;
       const scrollTop = this.document.body.scrollTop;
       this.scrollnormalize = (scrollMax - scrollTop) / scrollMax;
       this.globalService.scrollOpacity = 1.2 - this.scrollnormalize;
+      if(this.scrollnormalize <= 0) {
+        this.scrolldownDisplay = false;
+      }
   }
 
   public ngOnInit(): void {
+    window.scroll(0,0);
     this.activatedRoute.queryParams.subscribe(
       data => this.newUser = data.nU ? true : false
     );
@@ -35,6 +46,17 @@ export class homeComponent {
       }, 6000);
     }
   }
+  public ngAfterViewInit(): void {
+    const scrollTo: number = this.descriptionDom.nativeElement.offsetTop - 100;
+    $('.scroll-botttom').click(function(){
+        $('html, body').animate({ scrollTop: scrollTo }, 600);
+    });
+  }
+
+  public goTo(url: string): void {
+    this.router.navigateByUrl(url)
+  }
+
   public getWindowHeight(): string {
     return window.innerHeight +'px';
   }
