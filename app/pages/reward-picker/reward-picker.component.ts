1d55e6f10807c0b1bb94749355a0eee692b54c17
Chris Zepeda <chris.zeag@gmail.com>
2017-12-13 19:30:33 -0600

    webpack migrated from 1 to 2 as well as webpack-dev-server

--- app/pages/reward-picker/reward-picker.component.ts
+++ app/pages/reward-picker/reward-picker.component.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core';
 import {Router} from '@angular/router';
+import * as instaCam from 'instascan';
+// instaCam from https://github.com/schmich/instascan
 
 @Component({
   selector: 'reward-picker',
@@ -10,9 +12,25 @@ import {Router} from '@angular/router';
 export class RewardPickerComponent {
   public scanner: any;
   constructor() {
-    //console.log('instascan', instascan);
   }
   public ngOnInit(): void {
+    this.scanner = new instaCam.Scanner({ video: document.getElementById('preview')});
+    this.scanner.addListener('scan',(content: any) => {
+        if (content) {
+          console.log(content);
+          this.scanner.stop();
+        }
+      });
+    instaCam.Camera.getCameras().then((cameras: instaCam.Camera[]) => {
+        if (cameras.length > 0) {
+          this.scanner.start(cameras[0]);
+        } else {
+          console.error('No cameras found.');
+        }
+      }).catch((e: any) => {
+        console.error(e);
+      });
+
   }
 
   public ngAfterViewInit(): void {}
