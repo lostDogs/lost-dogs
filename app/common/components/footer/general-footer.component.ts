ace4ebd6ddd50dc4e00f8b8e9ed9acdfc34079a4
Chris Zepeda <chris.zeag@gmail.com>
2018-03-18 12:27:29 -0600

    setting url for social media

--- app/common/components/footer/general-footer.component.ts
+++ app/common/components/footer/general-footer.component.ts
@@ -1,5 +1,5 @@
 import { Component} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'general-footer',
@@ -8,7 +8,8 @@ import {ActivatedRoute} from '@angular/router';
 })
 export class generalFooterComponent {
   public newUser: boolean;
-  constructor (public activatedRoute: ActivatedRoute) {
+  constructor (public activatedRoute: ActivatedRoute, public router: Router) {
+
   }
   public ngOnInit(): void {
     this.activatedRoute.queryParams.subscribe(
@@ -16,8 +17,10 @@ export class generalFooterComponent {
     );
   }
 
-  public toPage(url: string): void {
-    window.open(url);
+  public toPage(url: string, appName: string): void {
+    setTimeout(() => { window.location.href = url}, 25);
+    window.location.href = appName;
+    // window.open(appName);
   }
 
 }
